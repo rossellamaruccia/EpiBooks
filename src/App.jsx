79be0420b3c5a7f5efb2c3fd0713924cf72b1be1
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-11-18 16:12:14 +0100

    exercise done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNav from "./MyNav"
 import MyFooter from "./MyFooter"
 import Welcome from './Welcome'
+import AllTheBooks from './AllTheBooks'
 
 const Home = function () {
   return (
     <>
       <MyNav />
       <Welcome />
+      <AllTheBooks />
       <MyFooter />
     </>
   )
